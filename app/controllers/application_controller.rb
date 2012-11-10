18d8066cc4e7cbff75fd2ca951db0bb83fa965fe
unknown <cquick@ITSM1033880.ACADEMIC.RRC.CA>
2012-11-10 15:57:20 -0600

    Added admin control to the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  #Admin control
+  protected
+  def require_sudo
+    require 'digest'
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && Digest::SHA1.hexdigest(password) == '6358b5cb2992b884678743e7ab0fdbb6e718a96f'
+    end
+  end
 end
