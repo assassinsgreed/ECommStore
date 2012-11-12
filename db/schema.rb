604c6499ad8904c4f18ed92a2589ec9a6be13c2f
unknown <cquick@ITSM1033880.ACADEMIC.RRC.CA>
2012-11-12 14:53:22 -0600

    Added a root route to point to the Products' index page, created product categories (1:Gold, 2:Gems, 3:Lost Artifacts, 4:Relics, 5:Other), Incorporated a partial into product views to remove redundancy, added images to the products model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110220753) do
+ActiveRecord::Schema.define(:version => 20121112202859) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20121110220753) do
     t.float    "sale_price"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "image"
   end
 
   create_table "provinces", :force => true do |t|
