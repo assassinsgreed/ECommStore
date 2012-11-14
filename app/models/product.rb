25d93109ddfdecc5052fe73904cb803009335f0e
cquick <cquick@academic.rrc.ca>
2012-11-14 10:01:00 -0600

    Added a root route to products#index, created categories(1:gold, 2:gems, 3:lost artifacts, 4:relics, 5:other), incorporated a partial into products view, and added images to products (sized at 75x75 pixels for thumbs, 300x300 for full image

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,6 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :description, :price, :quantity_on_hand
   validates :name, :uniqueness => true
   validates :image, :uniqueness => true
-  #sale price is not required.  If it is missing (null), the regular price is displayed
+  #sale price is not required.  If it is missing (null), the regular price is displayed 
   #Image is not required either.  If it is present, it must be unique though
 end
