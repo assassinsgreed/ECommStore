3eff157ebe18bfd9304b4692433ffb6ef1a1d80a
cquick <cquick@academic.rrc.ca>
2012-11-14 09:51:06 -0600

    Big Update

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,9 @@ class Product < ActiveRecord::Base
   belongs_to :line_item
   
   #Validations
-  validates_presence_of :name, :description, :price, :stock_quantity
+  validates_presence_of :name, :description, :price, :quantity_on_hand
   validates :name, :uniqueness => true
+  validates :image, :uniqueness => true
   #sale price is not required.  If it is missing (null), the regular price is displayed
+  #Image is not required either.  If it is present, it must be unique though
 end
