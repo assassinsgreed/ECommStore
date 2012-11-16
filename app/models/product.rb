471a43a18ef6a3e38582af95bded354c2089d33f
cquick <cquick@academic.rrc.ca>
2012-11-16 10:26:22 -0600

    Formatted an individual product's appearance, as well as removed the 'uniqueness' clause on a product's image as it made only one product able to use the 'Image not found' image

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,5 @@ class Product < ActiveRecord::Base
   #Validations
   validates_presence_of :name, :description, :price, :quantity_on_hand
   validates :name, :uniqueness => true
-  validates :image, :uniqueness => true
   #sale price is not required.  If it is missing (null), the regular price is displayed 
-  #Image is not required either.  If it is present, it must be unique though
 end
