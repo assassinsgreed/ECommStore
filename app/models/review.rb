134974d97240eb850e5aa84c9ba8c213e0542839
cquick <cquick@academic.rrc.ca>
2012-11-19 13:48:03 -0600

    Added some css styling to the reviews shown on a particular product

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,7 @@ class Review < ActiveRecord::Base
   
   #Validations
   validates_presence_of :body
+  validates :rating, :numericality => {:only_decimal => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5}
   
   #A method to ensure that the author either comes up as "Anonymous" or their author name, if given
   def attribution
